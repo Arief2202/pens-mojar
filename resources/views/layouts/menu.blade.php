6e83aed76fdc3c0fc23ab0a6393364d451928429
bayuhnm <bayuhidayahnurm@gmail.com>
2022-04-07 20:03:23 +0700

    first commit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,8 @@
+<li class="c-sidebar-nav-item">
+    <a class="c-sidebar-nav-link c-active" href="{{ route('home') }}">
+        <i class="c-sidebar-nav-icon cil-home"></i>Home
+    </a>
+    <a class="c-sidebar-nav-link c-active" href="#">
+        <i class="c-sidebar-nav-icon cil-user"></i>Test Aja
+    </a>
+</li>
