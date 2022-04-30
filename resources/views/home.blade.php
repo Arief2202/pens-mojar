108bf17dd47de8129936c107afac1b59e347002a
mukhlisbara <mukhlisbara12@gmail.com>
2022-05-01 00:34:55 +0700

    update tampilan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,308 @@
 @extends('layouts.app')
-
+@section('title', 'SUPER ADMIN | MOJAR APP')
 @section('content')
-Selamat menegerjakan gys
+<div class="container">
+    <h1 class="tittle-utama">Dashboard UP2AI</h1>
+    <h5 class="card-title tittle-login">Beranda</h5>
+    <br>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body px-5">
+                    <div class="card-columns">
+                        <div class="card mb-4 " style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title ">
+                                    <th scope="col">Teknik Informatika</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title">
+                                    <th scope="col">Teknik Komputer</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title">
+                                    <th scope="col">Data Sains</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4 " style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title ">
+                                    <th scope="col">Teknik Informatika</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title">
+                                    <th scope="col">Teknik Komputer</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title">
+                                    <th scope="col">Data Sains</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4 " style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title ">
+                                    <th scope="col">Teknik Informatika</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title">
+                                    <th scope="col">Teknik Komputer</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title">
+                                    <th scope="col">Data Sains</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4 " style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title ">
+                                    <th scope="col">Teknik Informatika</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title">
+                                    <th scope="col">Teknik Komputer</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                            <div class="card-header position-relative d-flex justify-content-center align-items-center text-white bg-dark">
+                                <div class="title">
+                                    <th scope="col">Data Sains</th>
+                                </div>
+                            </div>
+                            <div class="card-body row text-center">
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">12</div>
+                                    <div class="text-uppercase text-medium-emphasis small">praktikum</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">16</div>
+                                    <div class="text-uppercase text-medium-emphasis small">teori</div>
+                                </div>
+                                <div class="vr"></div>
+                                <div class="col">
+                                    <div class="fs-5 fw-semibold">8</div>
+                                    <div class="text-uppercase text-medium-emphasis small">interaktif</div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</div>    
+
 @endsection
