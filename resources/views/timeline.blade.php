6413cf46b64b72cdb11cfe39468759e019f1464c
LisaSanjaya <lisahidayatuss@gmail.com>
2022-05-03 12:35:50 +0700

    update timeline dan pengadaan

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -10,19 +10,35 @@
                             <div id="content">
                                 <ul class="timeline">
                                     <li class="event" data-date="12:30 - 1:00pm">
-                                        <h3>Registration</h3>
+                                        <h3>Deadline Pengumpulan Proposal</h3>
                                         <p>Get here on time, it's first come first serve. Be late, get turned away.</p>
                                     </li>
                                     <li class="event" data-date="2:30 - 4:00pm">
-                                        <h3>Opening Ceremony</h3>
+                                        <h3>Pengumpulan Kontrak yang sudah Ditandatangani</h3>
                                         <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP &amp; Busta Rhymes as an opening show.</p>
                                     </li>
                                     <li class="event" data-date="5:00 - 8:00pm">
-                                        <h3>Main Event</h3>
+                                        <h3>Pencairan Dana Tahap 1 (70%)</h3>
                                         <p>This is where it all goes down. You will compete head to head with your friends and rivals. Get ready!</p>
                                     </li>
                                     <li class="event" data-date="8:30 - 9:30pm">
-                                        <h3>Closing Ceremony</h3>
+                                        <h3>Pengerjaan</h3>
+                                        <p>See how is the victor and who are the losers. The big stage is where the winners bask in their own glory.</p>
+                                    </li>
+                                    <li class="event" data-date="8:30 - 9:30pm">
+                                        <h3>Deadline Pengumpulan Modul</h3>
+                                        <p>See how is the victor and who are the losers. The big stage is where the winners bask in their own glory.</p>
+                                    </li>
+                                    <li class="event" data-date="8:30 - 9:30pm">
+                                        <h3>Proses Review</h3>
+                                        <p>See how is the victor and who are the losers. The big stage is where the winners bask in their own glory.</p>
+                                    </li>
+                                    <li class="event" data-date="8:30 - 9:30pm">
+                                        <h3>Proses Revisi</h3>
+                                        <p>See how is the victor and who are the losers. The big stage is where the winners bask in their own glory.</p>
+                                    </li>
+                                    <li class="event" data-date="8:30 - 9:30pm">
+                                        <h3>Pencairan Dana Tahap II (30%)</h3>
                                         <p>See how is the victor and who are the losers. The big stage is where the winners bask in their own glory.</p>
                                     </li>
                                 </ul>
