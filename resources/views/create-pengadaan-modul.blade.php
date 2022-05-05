46a23fc2681c69216ee361e3fdab228c0b19f43e
LisaSanjaya <lisahidayatuss@gmail.com>
2022-05-05 23:08:01 +0700

    pengadaan

--- resources/views/create-pengadaan-modul.blade.php
+++ resources/views/create-pengadaan-modul.blade.php
@@ -1,50 +1,36 @@
 @extends('layouts.app')
-@section('title', 'MOJAR APP')
+@section('title', 'SUPER ADMIN | MOJAR APP')
 @section('content')
-    <div class="container">
-        <h1 class="tittle-utama">Tambah Akun</h1>
-        <h5 class="card-title tittle-login">Beranda \ <a href="/super-admin-kelola-akun">Kelola Akun</a> \ Tambah Akun</h5>
-        <br>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-body">
-                        <br>
-                        <div>
-                            <form>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">NIDN</label>
-                                    <input type="text" class="form-control" aria-descril>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Nama</label>
-                                    <input type="text" class="form-control" aria-descril>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Email</label>
-                                    <input type="text" class="form-control" aria-descril>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Password</label>
-                                    <input type="text" class="form-control" aria-descril>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Role</label>
-                                    <select class="form-control form-select" id="inputGroupSelect01">
-                                        <option selected> </option>
-                                        <option value="1">Isi Rolenya</option>
-                                        <option value="2">Isi Rolenya</option>
-                                        <option value="3">Isi Rolenya</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <button type="submit" class="btn btn-primary">Simpan</button>
-                                </div>
-                            </form>
-                        </div>
+<div class="container">
+    <h1 class="tittle-utama">Pengadaan Modul Ajar</h1>
+    <br>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="row">
+                    <div class="col-xl-4">
+                        <div class="card text-white bg-dark mb-3">
+                            <h5 class="card-header text-white">Surat Keterangan Penetapan Modul Ajar PENS</h5>
+                            <a class="btn btn-secondary" href="#" role="button">Upload</a>
+                         </div>
                     </div>
+                    <div class="col-xl-4">
+                        <div class="card text-white bg-dark mb-3">
+                            <h5 class="card-header text-white">Surat Keterangan Pencairan Dana Modul Ajar PENS</h5>
+                            <a class="btn btn-secondary" href="#" role="button">Upload</a>
+                         </div>
+                    </div>
+                    <div class="col-xl-4">
+                        <div class="card text-white bg-dark mb-3">
+                            <h5 class="card-header text-white">Buku Paedoman Pembuatan Modul Ajar PENS</h5>
+                            <button class="btn btn-secondary" type="button">Upload</button>
+                         </div>
+                    </div>
+                    
                 </div>
             </div>
-        </div>
+        </div> 
     </div>
+</div>    
+
 @endsection
